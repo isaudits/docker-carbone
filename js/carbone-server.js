f9907875dbaefc91fa02071f5f32ab99030687e3
Matthew C. Jones, CPA, CISA, OSCP, CCFE <mjones@isaudits.com>
2020-12-17 16:16:54 -0500

    bump up express limit to 50mb

--- js/carbone-server.js
+++ js/carbone-server.js
@@ -2,13 +2,14 @@ const carbone = require('carbone');
 const express = require('express');
 const app = express();
 
-app.use(express.urlencoded({ extended: false }))
-app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json({limit: '50mb'}));
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`)
-})
+});
 
 app.get('/', function(request, response) {
   
@@ -22,30 +23,30 @@ app.get('/', function(request, response) {
             </form>
         </body>
     </html>`
-  response.writeHead(200, {'Content-Type': 'text/html'})
-  response.end(html)
+  response.writeHead(200, {'Content-Type': 'text/html'});
+  response.end(html);
 })
 
 app.post('/', function(request, response) {
   
-  template = request.body.template
-  filename = template.replace(/^.*[\\\/]/, '')    //extract template filename to use as download file name
-  data = JSON.parse(request.body.json)
-  options = JSON.parse(request.body.options)
+  template = request.body.template;
+  filename = template.replace(/^.*[\\\/]/, '');    //extract template filename to use as download file name
+  data = JSON.parse(request.body.json);
+  options = JSON.parse(request.body.options);
 
   if (options.convertTo) {
     filename = filename.replace(/\.[^/.]+$/, "");   //change filename extension of converted (ie to PDF)
-    filename = filename+'.'+options.convertTo
+    filename = filename+'.'+options.convertTo;
   }
 
   carbone.render(template, data, options, function(err, result){
     if (err) {
       return console.log(err);
     }
-    response.setHeader('Content-Length', result.length)
-    response.setHeader('Content-Type', 'application/octet-stream')
+    response.setHeader('Content-Length', result.length);
+    response.setHeader('Content-Type', 'application/octet-stream');
     response.setHeader('Content-Disposition', 'attachment; filename="'+filename+'";');
-    response.send(result)
+    response.send(result);
   });
-})
+});
 
